db8e8b8d7ac0875804b3c9e4d8e34f3af29cb12a
AlexMFV <alexmfv1@gmail.com>
2019-10-15 18:31:55 +0100

    Fixed crash while drawing out of the grid (Out of Range)

--- CGOL/Canvas.h
+++ CGOL/Canvas.h
@@ -49,26 +49,33 @@ void DrawSquare(int mouseX, int mouseY) {
 }
 
 void DrawSquare(sf::Uint8* pixels, Cursor cur) {
-	int pixelX, pixelY;
+	try {
+		int pixelX, pixelY;
 
-	pixelX = floor(cur.GetX() / floor(m_width / gridSize));
-	pixelY = floor(cur.GetY() / floor((m_height-1) / gridSize));
+		pixelX = floor(cur.GetX() / floor(m_width / gridSize));
+		pixelY = floor(cur.GetY() / floor((m_height - 1) / gridSize));
 
-	if(pixelX >= 0 && pixelX <= floor(m_width / gridSize)
-		&& pixelY >= 0 && pixelY <= floor((m_height-1) / gridSize)) {
+		if (pixelX >= 0 && pixelX < m_width / floor(m_width / gridSize)
+			&& pixelY >= 0 && pixelY < (m_height - 1) / floor((m_height - 1) / gridSize)) {
 
-		int squareSize = floor(m_width / gridSize);
+			int squareSize = floor(m_width / gridSize);
 
-		for (int i = 0; i < squareSize; i++)
-		{
-			for (int j = 0; j < squareSize; j++)
+			for (int i = 0; i < squareSize; i++)
 			{
-				pixels[(pixelX * squareSize * 4) + (pixelY * squareSize * m_width * 4) + (i*4) + (j * m_width * 4)] = squareColor.r;
-				pixels[(pixelX * squareSize * 4) + (pixelY * squareSize * m_width * 4) + (i * 4) + (j * m_width * 4) + 1] = squareColor.g;
-				pixels[(pixelX * squareSize * 4) + (pixelY * squareSize * m_width * 4) + (i * 4) + (j * m_width * 4) + 2] = squareColor.b;
-				pixels[(pixelX * squareSize * 4) + (pixelY * squareSize * m_width * 4) + (i * 4) + (j * m_width * 4) + 3] = squareColor.a;
+				for (int j = 0; j < squareSize; j++)
+				{
+					pixels[(pixelX * squareSize * 4) + (pixelY * squareSize * m_width * 4) + (i * 4) + (j * m_width * 4)] = squareColor.r;
+					pixels[(pixelX * squareSize * 4) + (pixelY * squareSize * m_width * 4) + (i * 4) + (j * m_width * 4) + 1] = squareColor.g;
+					pixels[(pixelX * squareSize * 4) + (pixelY * squareSize * m_width * 4) + (i * 4) + (j * m_width * 4) + 2] = squareColor.b;
+					pixels[(pixelX * squareSize * 4) + (pixelY * squareSize * m_width * 4) + (i * 4) + (j * m_width * 4) + 3] = squareColor.a;
+				}
 			}
 		}
+	}
+	catch (const char* msg) {
+		std::cout << msg << std::endl;
+	}
+	
 		
 		//Maybe create a new array with window dimensions, identify the squares with a 1 or 0,
 		//after that, run the array and sum 4 to the pixels for each array access change colors,
@@ -102,7 +109,6 @@ void DrawSquare(sf::Uint8* pixels, Cursor cur) {
 		//}
 
 	}
-}
 
 Cursor GetMouseCoordinates(sf::RenderWindow& window, Cursor cursor) {
 	cursor.SetPos(sf::Mouse::getPosition(window));
